8bc733336dac4ccf8e69a116632be92aca1ecc41
rickparrish <github@rick.parrish>
2014-05-27 21:40:39 -0400

    Embed ANSIs and update game logic to use them

--- source/game.pas
+++ source/game.pas
@@ -9,8 +9,8 @@ procedure Start;
 implementation
 
 uses
-  Door,
-  StrUtils, SysUtils;
+  Door, StringUtils,
+  Classes, Crt, StrUtils, SysUtils;
 
 const
   BOARD_X: Integer = 25;
@@ -20,6 +20,9 @@ type
   TDirection = (dirDown, dirLeft, dirRight, dirUp);
 
 var
+  _AnsiGameBoard: String;
+  _AnsiNewTile: Array[1..4] of String;
+  _AnsiTile: Array[0..12] of String;
   _Board: Array[1..4, 1..4] of Integer;
   _PointsToAdd: Integer;
   _Score: Integer;
@@ -29,7 +32,10 @@ procedure DrawTiles; forward;
 
 procedure AddTile;
 var
- R, X, Y: Integer;
+  Line: Integer;
+  R: Integer;
+  Tile: Integer;
+  X, Y: Integer;
 begin
   // Pick a random location
   repeat
@@ -47,72 +53,48 @@ begin
     _Board[Y][X] := 4;
   end;
 
+  // animate "creation" of new tile
+  for Tile := 1 to 4 do
+  begin
+    DoorGotoXY((X - 1) * 19 + 4, (Y - 1) * 6 + 1);
+    DoorWrite(_AnsiNewTile[Tile]);
+    Delay(100);
+  end;
+
+  // Draw the actual tile
   DrawTile(X, Y);
 end;
 
 procedure DrawBoard;
-const
-  BottomLeft: Char = #192;
-  BottomRight: Char = #217;
-  LineLeft: Char = #195;
-  LineRight: Char = #180;
-  Shadow: Char = #219;
-  TileLeft: Char = #179;
-  TileRight: Char = #179;
-  TopLeft: Char = #218;
-  TopRight: Char = #191;
-var
-  RowLine: String;
-  RowTile: String;
-  Temp: String;
 begin
-  RowLine := StringOfChar(#196, 6);
-  Temp := StringOfChar(' ', 6);
-  RowTile := Temp + #179 + Temp + #179 + Temp + #179 + Temp;
-
-  DoorGotoXY(BOARD_X, BOARD_Y + 0);  DoorWrite('|07' + TopLeft + RowLine + #194 + RowLine + #194 + RowLine + #194 + RowLine + TopRight);
-  DoorGotoXY(BOARD_X, BOARD_Y + 1);  DoorWrite('|07' + TileLeft + RowTile + TileRight + '|08' + Shadow);
-  DoorGotoXY(BOARD_X, BOARD_Y + 2);  DoorWrite('|07' + TileLeft + RowTile + TileRight + '|08' + Shadow);
-  DoorGotoXY(BOARD_X, BOARD_Y + 3);  DoorWrite('|07' + TileLeft + RowTile + TileRight + '|08' + Shadow);
-  DoorGotoXY(BOARD_X, BOARD_Y + 4);  DoorWrite('|07' + LineLeft + RowLine + #197 + RowLine + #197 + RowLine + #197 + RowLine + LineRight + '|08' + Shadow);
-  DoorGotoXY(BOARD_X, BOARD_Y + 5);  DoorWrite('|07' + TileLeft + RowTile + TileRight + '|08' + Shadow);
-  DoorGotoXY(BOARD_X, BOARD_Y + 6);  DoorWrite('|07' + TileLeft + RowTile + TileRight + '|08' + Shadow);
-  DoorGotoXY(BOARD_X, BOARD_Y + 7);  DoorWrite('|07' + TileLeft + RowTile + TileRight + '|08' + Shadow);
-  DoorGotoXY(BOARD_X, BOARD_Y + 8);  DoorWrite('|07' + LineLeft + RowLine + #197 + RowLine + #197 + RowLine + #197 + RowLine + LineRight + '|08' + Shadow);
-  DoorGotoXY(BOARD_X, BOARD_Y + 9);  DoorWrite('|07' + TileLeft + RowTile + TileRight + '|08' + Shadow);
-  DoorGotoXY(BOARD_X, BOARD_Y + 10); DoorWrite('|07' + TileLeft + RowTile + TileRight + '|08' + Shadow);
-  DoorGotoXY(BOARD_X, BOARD_Y + 11); DoorWrite('|07' + TileLeft + RowTile + TileRight + '|08' + Shadow);
-  DoorGotoXY(BOARD_X, BOARD_Y + 12); DoorWrite('|07' + LineLeft + RowLine + #197 + RowLine + #197 + RowLine + #197 + RowLine + LineRight + '|08' + Shadow);
-  DoorGotoXY(BOARD_X, BOARD_Y + 13); DoorWrite('|07' + TileLeft + RowTile + TileRight + '|08' + Shadow);
-  DoorGotoXY(BOARD_X, BOARD_Y + 14); DoorWrite('|07' + TileLeft + RowTile + TileRight + '|08' + Shadow);
-  DoorGotoXY(BOARD_X, BOARD_Y + 15); DoorWrite('|07' + TileLeft + RowTile + TileRight + '|08' + Shadow);
-  DoorGotoXY(BOARD_X, BOARD_Y + 16); DoorWrite('|07' + BottomLeft + RowLine + #193 + RowLine + #193 + RowLine + #193 + RowLine + BottomRight + '|08' + Shadow);
-  DoorGotoXY(BOARD_X, BOARD_Y + 17); DoorWrite(' |08' + StringOfChar(Shadow,  29) + '|07');
+  DoorTextBackground(Crt.Blue);
+  DoorClrScr;
+  DoorWrite(_AnsiGameBoard);
 end;
 
 procedure DrawScore;
 begin
   if (_PointsToAdd > 0) then
   begin
-    DoorGotoXY(2, 5);
-    DoorWrite('|0A' + AddChar(' ', '+' + IntToStr(_PointsToAdd), 6));
-
     _Score += _PointsToAdd;
+  end;
+
+  DoorGotoXY(2, 24);
+  DoorWrite('|1FScore: ');
+  DoorWrite('|1E' + IntToStr(_Score));
+
+  if (_PointsToAdd > 0) then
+  begin
+    DoorWrite(' |1A' + AddCharR(' ', '+' + IntToStr(_PointsToAdd), 6));
     _PointsToAdd := 0;
   end else
   begin
-    DoorGotoXY(2, 5);
     DoorWrite(StringOfChar(' ', 6));
   end;
-
-  DoorGotoXY(2, 2); DoorWrite('|0FScore:');
-  DoorGotoXY(2, 3); DoorWrite('|08' + StringOfChar(#196, 6));
-  DoorGotoXY(2, 4); DoorWrite('|0F' + AddChar(' ', IntToStr(_Score), 6) + '|07');
 end;
 
 procedure DrawScreen;
 begin
-  DoorClrScr;
   DrawBoard;
   DrawTiles;
   DrawScore;
@@ -120,15 +102,27 @@ end;
 
 procedure DrawTile(X, Y: Integer);
 var
-  S: String;
+  Line: Integer;
+  Tile: Integer;
 begin
-  S := IntToStr(_Board[Y][X]);
-  if (S = '0') then
-  begin
-    S := '    ';
+  case _Board[Y][X] of
+    0: Tile := 0;
+    1: Tile := 1;
+    2: Tile := 2;
+    4: Tile := 3;
+    8: Tile := 4;
+    16: Tile := 5;
+    32: Tile := 6;
+    64: Tile := 7;
+    128: Tile := 8;
+    256: Tile := 9;
+    512: Tile := 10;
+    1024: Tile := 11;
+    2048: Tile := 12;
   end;
-  DoorGotoXY((BOARD_X + 2) + ((X - 1) * 7) + ((4 - Length(S)) div 2), (BOARD_Y + 2) + ((Y - 1) * 4));
-  DoorWrite('|0F' + S + '|07');
+
+  DoorGotoXY((X - 1) * 19 + 4, (Y - 1) * 6 + 1);
+  DoorWrite(_AnsiTile[Tile]);
 end;
 
 procedure DrawTiles;
@@ -346,6 +340,28 @@ begin
   end;
 end;
 
+procedure Init;
+begin
+  _AnsiGameBoard := Base64Decode('G1swbRtbNDQ7MzBtICAg3Nvf39/f39/f39/f39/f29wbWzE7MzdtICAbWzA7NDQ7MzBt3Nvf39/f39/f39/f39/f29wbWzE7MzdtICAbWzA7NDQ7MzBt3Nvf39/f39/f39/f39/f29wbWzE7MzdtICAbWzA7NDQ7MzBt3Nvf39/f39/f39/f39/f29wbWzE7MzdtIBtbMDs0NDszMG0gG1sxOzM3bSAbWzA7NDQ7MzBtIBtbMTszN20gG1swOzQ0OzMwbSDbICAgICAgICAgICAgICAg2xtbMTszN20gIBtbMDs0NDszMG3bICAgICAgICAgICAgICAg2xtbMTszN20gIBtbMDs0NDszMG3bICAgICAgICAgICAgICAg2xtbMTszN20gIBtbMDs0NDszMG3bICAgICAgICAgICAgICAg2xtbMTszN20gG1swOzQ0OzMwbSAbWzE7MzdtIBtbMDs0NDszMG0gG1sxOzM3bSAbWzA7NDQ7MzBtINsgICAgICAgICAgICAgICDbG1sxOzM3bSAgG1swOzQ0OzMwbdsgICAgICAgICAgICAgICDbG1sxOzM3bSAgG1swOzQ0OzMwbdsgICAgICAgICAgICAgICDbG1sxOzM3bSAgG1swOzQ0OzMwbdsgICAgICAgICAgICAgICDbG1sxOzM3bSAbWzA7NDQ7MzBtIBtbMTszN20gG1swOzQ0OzMwbSAbWzE7MzdtIBtbMDs0NDszMG0g2yAgICAgICAgICAgICAgINsbWzE7MzdtICAbWzA7NDQ7MzBt2yAgICAgICAgICAgICAgINsbWzE7MzdtICAbWzA7NDQ7MzBt2yAgICAgICAgICAgICAgINsbWzE7MzdtICAbWzA7NDQ7MzBt2yAgICAgICAgICAgICAgINsbWzE7MzdtIBtbMDs0NDszMG0gG1sxOzM3bSAbWzA7NDQ7MzBtIBtbMTszN20gG1swOzQ0OzMwbSDf29zc3Nzc3Nzc3Nzc3Nzb3xtbMTszN20gIBtbMDs0NDszMG3f29zc3Nzc3Nzc3Nzc3Nzb3xtbMTszN20gIBtbMDs0NDszMG3f29zc3Nzc3Nzc3Nzc3Nzb3xtbMTszN20gIBtbMDs0NDszMG3f29zc3Nzc3Nzc3Nzc3Nzb3xtbMTszN20gG1swOzQ0OzMwbSAbWzE7MzdtIBtbMDs0NDszMG0gICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgINzb39/f39/f39/f39/f39vcICDc29/f39/f39/f39/f39/b3CAg3Nvf39/f39/f39/f39/f29wgINzb39/f39/f39/f39/f39vcICAgICAg2yAgICAgICAgICAgICAgINsgINsgICAgICAgICAgICAgICDbICDbICAgICAgICAgICAgICAg2yAg2yAgICAgICAgICAgICAgINsgICAgICDbICAgICAgICAgICAgICAg2yAg2yAgICAgICAgICAgICAgINsgINsgICAgICAgICAgICAgICDbICDbICAgICAgICAgICAgICAg2yAgICAgINsgICAgICAgICAgICAgICDbICDbICAgICAgICAgICAgICAg2yAg2yAgICAgICAgICAgICAgINsgINsgICAgICAgICAgICAgICDbICAgICAg39vc3Nzc3Nzc3Nzc3Nzc298gIN/b3Nzc3Nzc3Nzc3Nzc3NvfICDf29zc3Nzc3Nzc3Nzc3Nzb3yAg39vc3Nzc3Nzc3Nzc3Nzc298gICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgINzb39/f39/f39/f39/f39vcICDc29/f39/f39/f39/f39/b3CAg3Nvf39/f39/f39/f39/f29wgINzb39/f39/f39/f39/f39vcICAgICAg2yAgICAgICAgICAgICAgINsgINsgICAgICAgICAgICAgICDbICDbICAgICAgICAgICAgICAg2yAg2yAgICAgICAgICAgICAgINsgICAgICDbICAgICAgICAgICAgICAg2yAg2yAgICAgICAgICAgICAgINsgINsgICAgICAgICAgICAgICDbICDbICAgICAgICAgICAgICAg2yAgICAgINsgICAgICAgICAgICAgICDbICDbICAgICAgICAgICAgICAg2yAg2yAgICAgICAgICAgICAgINsgINsgICAgICAgICAgICAgICDbICAgICAg39vc3Nzc3Nzc3Nzc3Nzc298gIN/b3Nzc3Nzc3Nzc3Nzc3NvfICDf29zc3Nzc3Nzc3Nzc3Nzb3yAg39vc3Nzc3Nzc3Nzc3Nzc298gICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgINzb39/f39/f39/f39/f39vcICDc29/f39/f39/f39/f39/b3CAg3Nvf39/f39/f39/f39/f29wgINzb39/f39/f39/f39/f39vcICAgICAg2yAgICAgICAgICAgICAgINsgINsgICAgICAgICAgICAgICDbICDbICAgICAgICAgICAgICAg2yAg2yAgICAgICAgICAgICAgINsgICAgICDbICAgICAgICAgICAgICAg2yAg2yAgICAgICAgICAgICAgINsgINsgICAgICAgICAgICAgICDbICDbICAgICAgICAgICAgICAg2yAgICAgINsgICAgICAgICAgICAgICDbICDbICAgICAgICAgICAgICAg2yAg2yAgICAgICAgICAgICAgINsgINsgICAgICAgICAgICAgICDbICAgICAg39vc3Nzc3Nzc3Nzc3Nzc298gIN/b3Nzc3Nzc3Nzc3Nzc3NvfICDf29zc3Nzc3Nzc3Nzc3Nzb3yAg39vc3Nzc3Nzc3Nzc3Nzc298gICA=');
+  _AnsiNewTile[1] := Base64Decode('G1tzG1swbRtbNDQ7MzBt3Nvf39/f39/f39/f39/f29wbWzBtDQobW3UbW0IbWzQ0OzMwbdsgICAgICAgICAgICAgICDbG1swbQ0KG1t1G1syQhtbNDQ7MzBt2yAgICAgIBtbMTs0MG2xsbEbWzA7NDQ7MzBtICAgICAg2xtbMG0NChtbdRtbM0IbWzQ0OzMwbdsgICAgICAgICAgICAgICDbG1swbQ0KG1t1G1s0QhtbNDQ7MzBt39vc3Nzc3Nzc3Nzc3Nzc298bWzBtDQo=');
+  _AnsiNewTile[2] := Base64Decode('G1tzG1swbRtbNDQ7MzBt3Nvf39/f39/f39/f39/f29wbWzBtDQobW3UbW0IbWzQ0OzMwbdsgICAgIBtbMTs0MG2ysrKyshtbMDs0NDszMG0gICAgINsbWzBtDQobW3UbWzJCG1s0NDszMG3bICAgICAbWzE7NDBtsrKysrIbWzA7NDQ7MzBtICAgICDbG1swbQ0KG1t1G1szQhtbNDQ7MzBt2yAgICAgICAgICAgICAgINsbWzBtDQobW3UbWzRCG1s0NDszMG3f29zc3Nzc3Nzc3Nzc3Nzb3xtbMG0NCg==');
+  _AnsiNewTile[3] := Base64Decode('G1tzG1swbRtbNDQ7MzBt3Nvf39/f39/f39/f39/f29wbWzBtDQobW3UbW0IbWzQ0OzMwbdsgIBtbMTs0N22xsbGxsbGxsbGxsRtbMDs0NDszMG0gINsbWzBtDQobW3UbWzJCG1s0NDszMG3bICAbWzE7NDdtsbGxsbGxsbGxsbEbWzA7NDQ7MzBtICDbG1swbQ0KG1t1G1szQhtbNDQ7MzBt2yAgG1sxOzQ3bbGxsbGxsbGxsbGxG1swOzQ0OzMwbSAg2xtbMG0NChtbdRtbNEIbWzQ0OzMwbd/b3Nzc3Nzc3Nzc3Nzc3NvfG1swbQ0K');
+  _AnsiNewTile[4] := Base64Decode('G1tzG1swbRtbMTs0NzszMG0gICAgICAgICAgICAgICAgIBtbMG0NChtbdRtbQhtbMTs0NzszMG0gICAgICAgICAgICAgICAgIBtbMG0NChtbdRtbMkIbWzE7NDc7MzBtICAgICAgICAgICAgICAgICAbWzBtDQobW3UbWzNCG1sxOzQ3OzMwbSAgICAgICAgICAgICAgICAgG1swbQ0KG1t1G1s0QhtbMTs0NzszMG3c3Nzc3Nzc3Nzc3Nzc3Nzc3BtbMG0NCg==');
+  _AnsiTile[0] := Base64Decode('G1tzG1s0NDszMG3c29/f39/f39/f39/f39/b3A0KG1t1G1tC2yAgICAgICAgICAgICAgINsNChtbdRtbMkLbICAgICAgICAgICAgICAg2w0KG1t1G1szQtsgICAgICAgICAgICAgICDbDQobW3UbWzRC39vc3Nzc3Nzc3Nzc3Nzc298bWzBtDQo=');
+  _AnsiTile[1] := Base64Decode('G1tzG1swbRtbMW3b29vb29vb39/b29vb29vb2w0KG1t1G1tC29vb29vb29sg29vb29vb29sNChtbdRtbMkLb29vb29vb2yDb29vb29vb2w0KG1t1G1szQtvb29vb29vfIN/b29vb29vbDQobW3UbWzRCG1s0N23f39/f39/f39/f39/f39/f3xtbMG0=');
+  _AnsiTile[2] := Base64Decode('G1tzG1swbRtbMW3b29vb29vb39/b29vb29vb2w0KG1t1G1tC29vb29vb29vbINvb29vb29sNChtbdRtbMkLb29vb29vb39zb29vb29vb2w0KG1t1G1szQtvb29vb29vc39/b29vb29vbDQobW3UbWzRCG1s0N23f39/f39/f39/f39/f39/f3xtbMG0=');
+  _AnsiTile[3] := Base64Decode('G1tzG1swbRtbMW3b29vb29vb39vb29vb29vb2w0KG1t1G1tC29vb29vb2yDbINvb29vb29sNChtbdRtbMkLb29vb29vb3Nwg29vb29vb2w0KG1t1G1szQtvb29vb29vb2yDb29vb29vbDQobW3UbWzRCG1s0N23f39/f39/f39/f39/f39/f3xtbMG0=');
+  _AnsiTile[4] := Base64Decode('G1tzG1swbRtbMW3b29vb29vb39/b29vb29vb2w0KG1t1G1tC29vb29vb2yDbINvb29vb29sNChtbdRtbMkLb29vb29vb39zf29vb29vb2w0KG1t1G1szQtvb29vb29vc3yDb29vb29vbDQobW3UbWzRCG1s0N23f39/f39/f39/f39/f39/f3xtbMG0=');
+  _AnsiTile[5] := Base64Decode('G1tzG1swbRtbMW3b29vb29/f29vb39/b29vb2w0KG1t1G1tC29vb29vbINvbINvc29vb29sNChtbdRtbMkLb29vb29sg29sg3N/b29vb2w0KG1t1G1szQtvb29vb3yDf29zf3Nvb29vbDQobW3UbWzRCG1s0N23f39/f39/f39/f39/f39/f3xtbMG0=');
+  _AnsiTile[6] := Base64Decode('G1tzG1swbRtbMW3b29vb29/f29vf39vb29vb2w0KG1t1G1tC29vb29vb2yDb29sg29vb29sNChtbdRtbMkLb29vb29vc39vf3Nvb29vb2w0KG1t1G1szQtvb29vb39/c29zf39vb29vbDQobW3UbWzRCG1s0N23f39/f39/f39/f39/f39/f3xtbMG0=');
+  _AnsiTile[7] := Base64Decode('G1tzG1swbRtbMW3b29vb29vf39vf29vb29vb2w0KG1t1G1tC29vb29sg29zbINsg29vb29sNChtbdRtbMkLb29vb2yDc39vc3CDb29vb2w0KG1t1G1szQtvb29vb3N/c29vbINvb29vbDQobW3UbWzRCG1s0N23f39/f39/f39/f39/f39/f3xtbMG0=');
+  _AnsiTile[8] := Base64Decode('G1tzG1swbRtbMW3b29vf39vb39/b29/f29vb2w0KG1t1G1tC29vb2yDb29vbINsg2yDb29sNChtbdRtbMkLb29vbINvb39zb29/c39vb2w0KG1t1G1szQtvb298g39vc39/b3N8g29vbDQobW3UbWzRCG1s0N23f39/f39/f39/f39/f39/f3xtbMG0=');
+  _AnsiTile[9] := Base64Decode('G1tzG1swbRtbMW3b29vf39vb39/b29vf39vb2w0KG1t1G1tC29vb29sg2yDb29sg29zb29sNChtbdRtbMkLb29vf3Nvb29zf2yDc39vb2w0KG1t1G1szQtvb29zf39vf39zb3N/c29vbDQobW3UbWzRCG1s0N23f39/f39/f39/f39/f39/f3xtbMG0=');
+  _AnsiTile[10] := Base64Decode('G1tzG1swbRtbMW3b29vf39vb39/b29/f29vb2w0KG1t1G1tC29vbINvb29sg29vb2yDb29sNChtbdRtbMkLb29vb3N/b2yDb29/c29vb2w0KG1t1G1szQtvb29/f3NvfIN/b3N/f29vbDQobW3UbWzRCG1s0N23f39/f39/f39/f39/f39/f3xtbMG0=');
+  _AnsiTile[11] := Base64Decode('G1tzG1swbRtbMW3b39/b29vf39vf39vb39vb2w0KG1t1G1tC29sg29sg2yDb29sg2yDbINsNChtbdRtbMkLb2yDb2yDbINvf3Nvb3Nwg2w0KG1t1G1szQtvfIN/bIN/c29zf39vb2yDbDQobW3UbWzRCG1s0N23f39/f39/f39/f39/f39/f3xtbMG0=');
+  _AnsiTile[12] := Base64Decode('G1tzG1swbRtbMW3b39/b29vf39vf29vb39/b2w0KG1t1G1tC29vbINsg2yDbINsg2yDbINsNChtbdRtbMkLb39zb2yDbINvc3CDb39zf2w0KG1t1G1szQtvc39/bIN/c29vbINvc3yDbDQobW3UbWzRCG1s0N23f39/f39/f39/f39/f39/f3xtbMG0=');
+end;
+
 procedure NewGame;
 var
   X, Y: Integer;
@@ -354,7 +370,7 @@ begin
   _PointsToAdd := 0;
   _Score := 0;
 
-  // Reset board and add start tiles
+  // Reset board
   for Y := 1 to 4 do
   begin
     for X := 1 to 4 do
@@ -362,11 +378,13 @@ begin
       _Board[Y][X] := 0;
     end;
   end;
-  AddTile;
-  AddTile;
 
   // Draw screen
   DrawScreen;
+
+  // Add initial tiles
+  AddTile;
+  AddTile;
 end;
 
 procedure Start;
@@ -374,12 +392,12 @@ var
   Ch: Char;
   Moved: Boolean;
 begin
+  Init;
   NewGame;
 
   Ch := #0;
   repeat
     Moved := false;
-    DoorGotoXY(1, 1);
 
     // TODO Handle arrow keys (RMDoor)
     Ch := UpCase(DoorReadKey);
