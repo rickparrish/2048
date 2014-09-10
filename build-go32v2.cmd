e78fcfbfbead0e472e09729682e59a22c39964c1
rickparrish <github@rick.parrish>
2014-09-10 09:34:55 -0400

    Wouldn't build with -Mobjfpc anymore for some reason

--- build-go32v2.cmd
+++ build-go32v2.cmd
@@ -2,5 +2,5 @@
 z:
 cd \programming\2048\source
 rem fpc -MObjFPC -Scghi -O1 -g -gl -vewnhi -Fi..\obj\i386-go32v2 -Fu..\..\RMDoor -Fu. -FU..\obj\i386-go32v2\ -l -FE..\bin\i386-go32v2\ -o2048.exe BBS2048.lpr
-fpc -MObjFPC -Scghi -O1 -g -Fi..\obj\i386-go32v2 -Fu..\..\RMDoor -Fu. -FU..\obj\i386-go32v2\ -FE..\bin\i386-go32v2\ BBS2048.lpr
+fpc -Scghi -O1 -g -Fi..\obj\i386-go32v2 -Fu..\..\RMDoor -Fu. -FU..\obj\i386-go32v2\ -FE..\bin\i386-go32v2\ BBS2048.lpr
 pause
